18e04bdbd4e37599d3c796c431da99059b392040
Erkesh <dedeeverkesh@gmail.com>
2022-10-02 00:13:29 +0300

    Added AuthenticationFragment

--- app/src/main/java/my/project/techtestapp/presentation/MainActivity.kt
+++ app/src/main/java/my/project/techtestapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package my.project.techtestapp
+package my.project.techtestapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import my.project.techtestapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
