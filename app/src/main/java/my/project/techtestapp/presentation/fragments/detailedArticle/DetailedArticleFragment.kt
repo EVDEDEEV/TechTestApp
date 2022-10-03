ad3b08d0ca282d0c26f14681bc5ffa7d7bca9087
Erkesh <dedeeverkesh@gmail.com>
2022-10-03 03:45:08 +0300

    Added DetailedArticleFragment.kt

---
+++ app/src/main/java/my/project/techtestapp/presentation/fragments/detailedArticle/DetailedArticleFragment.kt
@@ -0,0 +1,13 @@
+package my.project.techtestapp.presentation.fragments.detailedArticle
+
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import my.project.techtestapp.R
+import my.project.techtestapp.databinding.FragmentDetailedArticleBinding
+
+
+class DetailedArticleFragment : Fragment(R.layout.fragment_detailed_article) {
+
+    private val binding by viewBinding(FragmentDetailedArticleBinding::bind)
+
+}
