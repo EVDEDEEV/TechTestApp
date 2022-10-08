dff4beae964f89215a96174d8c188d5acec84fe5
Erkesh <dedeeverkesh@gmail.com>
2022-10-08 22:24:58 +0300

    Moved some operations which works with network from AppModule to NetworkModule

---
+++ app/src/main/java/my/project/techtestapp/app/Application.kt
@@ -0,0 +1,19 @@
+package my.project.techtestapp.app
+
+import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+@HiltAndroidApp
+class Application : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+     override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
---
+++ app/src/main/java/my/project/techtestapp/di/NetworkModule.kt
@@ -0,0 +1,53 @@
+package my.project.techtestapp.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import my.project.techtestapp.data.api.ArticlesResponseApi
+import my.project.techtestapp.data.api.AuthenticationApi
+import my.project.techtestapp.utils.Constants
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    fun httpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+
+
+    @Provides
+    fun provideRatesApiInterface(retrofit: Retrofit): AuthenticationApi {
+        return retrofit.create(AuthenticationApi::class.java)
+    }
+
+
+    @Provides
+    fun provideArticlesInterface(retrofit: Retrofit): ArticlesResponseApi {
+        return retrofit.create(ArticlesResponseApi::class.java)
+    }
+
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor())
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
